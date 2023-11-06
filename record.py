eaac1dcdde54f7d65ece765f19d09d445cd0f10c
Pratyush <msgpratyush@gmail.com>
2023-11-06 18:19:35 +0000

    adding readme along with main files

---
+++ record.py
@@ -0,0 +1,47 @@
+import pyaudio
+import wave
+import time
+
+def rec(output_file, duration):
+    current = round(time.time() * 1000)
+    # Parameters for audio recording
+    FORMAT = pyaudio.paInt16  # Sample format
+    CHANNELS = 1              # Number of audio channels (1 for mono, 2 for stereo)
+    RATE = 44100              # Sample rate (samples per second)
+    RECORD_SECONDS = duration       # Duration of the recording in seconds
+    OUTPUT_FILE = output_file  # Output WAV file
+
+    # Initialize PyAudio
+    audio = pyaudio.PyAudio()
+
+    # Create an audio stream
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=1024)
+
+    #print("Recording..." + output_file)
+    print("Started Recording " + output_file, 1699270000000 - round(time.time() * 1000))
+    frames = []
+
+    # Record audio
+    for i in range(0, int(RATE / 1024 * RECORD_SECONDS)):
+        data = stream.read(1024)
+        frames.append(data)
+
+    #print("Finished recording." + output_file)
+    print("Ended Recording" + output_file, 1699270000000 - round(time.time() * 1000))
+
+    # Stop and close the audio stream
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # Save the recorded audio to a WAV file
+    with wave.open(OUTPUT_FILE, 'wb') as wf:
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(audio.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+
+    #print(f"Audio saved to {OUTPUT_FILE}")
+    
---
+++ test.py
@@ -0,0 +1,220 @@
+import os
+import record
+import re
+import threading
+import speech_recognition as sr
+import requests
+import json
+from pydub import AudioSegment
+import time
+import sys
+import tts
+
+
+if sys.version_info[0] < 3:
+	#for python2.7:
+	from Queue import Queue
+else:
+	#python python 3+:
+	from queue import Queue
+
+api_url = "http://13.42.27.138:8080"
+api_key = "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"
+
+def combine(audio_file1, audio_file2, output_file):
+
+    audio1 = AudioSegment.from_wav(audio_file1)
+    audio2 = AudioSegment.from_wav(audio_file2)
+
+    combined_audio = audio1 + audio2
+    combined_audio = combined_audio.set_channels(1)
+
+    combined_audio.export(output_file, format="wav")
+
+def mono(input_file, output_file):
+	
+	sound = AudioSegment.from_wav(input_file)
+	sound = sound.set_channels(1)
+	sound.export(output_file, format="wav")
+
+def ask(user_input):
+
+    # Create a dictionary with the question
+    data = {
+        "question": user_input,
+        "api key": api_key
+    }
+
+
+    # Encode the data as JSON
+    data_json = json.dumps(data)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    # Send a POST request to the API to ask the question
+    req = requests.post(api_url+"/ask", data=data_json, headers=headers)
+
+    # Print the answer
+    return req.json()["answer"]
+
+def scribe(path):
+    files = {
+        'audio_file': (path, open(path, 'rb'), 'audio/wav'),
+        'text': (None, api_key)
+        }
+
+    response = requests.post(api_url+"/listen", files=files)
+    return response.json()["transcript"]
+
+def check(file, hyperaware=False, curious=False):
+	r = sr.Recognizer()
+	r.energy_threshold = 1500
+	r.language = 'en-US'
+	# print("Check started at:", time.ctime(time.time()))
+	# Initialize the recognizer
+	with sr.WavFile(file) as source:
+		#use "test.wav" as the audio source
+		audio = r.record(source)
+	try:
+		words = r.recognize(audio)
+
+		print("I heard: ")
+		print(words)
+		
+		if hyperaware:
+			whisper_words = set(re.split(r"[, .]+", scribe(file)["text"]))
+			##print(whisper_words)
+			for word in whisper_words:
+				if re.findall(r"(pep|per)", word, re.IGNORECASE):
+					print("awake Check ended at:", time.ctime(time.time()))
+					return "awake"
+			print("not awake Check ended at:", time.ctime(time.time()))
+			return "heard asleep"
+		else:
+			words = set(re.split(r"[, .]+", words))
+			##print(words)
+			if curious:
+				whisper_words = set(re.split(r"[, .]+", scribe(file)["text"]))
+				##print(whisper_words)
+
+			for word in words:
+				if re.findall(r"(pep|per)", word, re.IGNORECASE):
+					print("Awake at: ", time.ctime(time.time()))
+					return "awake"
+			print("Heard at: ", time.ctime(time.time()))
+			return "heard"
+
+
+	except LookupError:
+		print("Silent at:", time.ctime(time.time()))
+		return "silence"
+
+def checker(file, q, word, output_file=None):
+
+	value = check(file) == word
+	q.put(value)
+
+	if word=="silence":
+
+		if value:
+			os.rename(output_file, output_file)
+
+		else:
+			combine(output_file,'3.wav',output_file)
+
+def threader(target1, args1, target2, args2):
+
+	thread1 = threading.Thread(target=target1, args=args1)
+	thread2 = threading.Thread(target=target2, args=args2)
+
+	thread1.start()
+	thread2.start()
+
+	thread1.join()
+	thread2.join()
+
+	mono('3.wav', '3.wav')
+
+def record_heys(output_file):
+
+	record.rec("1.wav", 4)
+	record.rec("2.wav", 4)
+	combine('1.wav','2.wav', output_file)
+
+	awake_queue = Queue()
+	silence_queue = Queue()
+
+	while True:
+
+		threader(checker, (output_file, awake_queue,"awake"), record.rec, ('3.wav',4,))
+
+		if awake_queue.get():
+
+			while True:
+
+				threader(checker, ('3.wav',silence_queue,"silence",output_file), record.rec, ('3.wav',5,))
+
+				if silence_queue.get():
+
+					break
+
+			break
+
+		else:
+
+			os.remove("1.wav")
+			os.rename("2.wav", "1.wav")
+			os.rename("3.wav", "2.wav")
+			combine('1.wav','2.wav', output_file)
+
+def cleanup(path):
+
+	files = ["1.wav", "2.wav", "3.wav", "cf.wav", "speech.mp3", path]
+
+	for file in files:
+		if os.path.exists(file):
+			os.remove(file)
+	
+	# # Stop and close the audio stream
+	# record.stream.stop_stream()
+	# record.stream.close()
+	# record.audio.terminate()
+	
+	print("cleanup done")
+
+if __name__ == "__main__":
+
+	while True:
+
+		path = "full.wav"
+
+		record_heys(path)
+
+		try:
+			question = re.split(r"[, .]+", scribe(path)["text"])
+			position = 0
+			for i in range(len(question)):
+				if re.findall(r"(pep|per)", question[i], re.IGNORECASE):
+					position = i+1
+					break
+
+			question = "User: "+ " ".join(question[position:])
+			print(question)
+			answer = ask(question)
+			print(answer)
+			tts.speak(answer)
+
+		except requests.exceptions.ConnectionError:
+			r = sr.Recognizer()
+			r.energy_threshold = 1500
+			r.language = 'en-US'
+			print("Could not connect to API")
+			with sr.WavFile(path) as source:
+				audio = r.record(source)
+			words = r.recognize(audio)
+			print("Google thinks you said: " + words)
+
+		cleanup(path)
+
+
