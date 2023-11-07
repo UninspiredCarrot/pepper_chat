9808fee3b5ba5fb3c6a0c4e0bf76ee85c976a8dc
Pratyush <msgpratyush@gmail.com>
2023-11-07 14:59:07 +0000

    change to path variable, so it works everywhere

--- test.py
+++ test.py
@@ -156,7 +156,7 @@ def record_heys(output_file):
 
 			while True:
 
-				threader(checker, (record_path + '3.wav',silence_queue,"silence",output_file), record.rec, (record_path + '3.wav',8,))
+				threader(checker, (record_path + '3.wav',silence_queue,"silence",output_file), record.rec, (record_path + '3.wav',6,))
 
 				if silence_queue.get():
 
